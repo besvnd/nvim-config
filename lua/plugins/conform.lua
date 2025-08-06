9df6c9cbc93f96bc547a3bd703d0690ebbb002c2
Bendik Svendsen <bendik.svendsen@fremtind.no>
2025-08-06 16:30:50 +0200

    use palantir-java-format with conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,11 +1,17 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    opts = function(_, opts)
-      opts.formatters_by_ft = vim.tbl_extend("force", opts.formatters_by_ft or {}, {
-        java = { "google-java-format" },
-      })
-      return opts
-    end,
-  },
+  "stevearc/conform.nvim",
+  opts = function(_, opts)
+    opts.formatters_by_ft = vim.tbl_extend("force", opts.formatters_by_ft or {}, {
+      java = { "palantir_java_format" },
+    })
+
+    opts.formatters = opts.formatters or {}
+    opts.formatters.palantir_java_format = {
+      command = vim.fn.expand("~/opt/bin/palantir-java-format"),
+      args = { "--replace", "$FILENAME" },
+      stdin = false,
+    }
+
+    return opts
+  end,
 }
