ecf8dcf46d4b263e20a2e7509f8433813a6e8629
Bendik Svendsen <bendik.svendsen@fremtind.no>
2025-08-28 14:16:50 +0200

    add dap config

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -3,3 +3,17 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.expandtab = true
 vim.opt_local.autoindent = true
 vim.opt_local.smarttab = true
+
+local ok, dap = pcall(require, "dap")
+if not ok then
+  return
+end
+
+dap.configurations.java = dap.configurations.java or {}
+table.insert(dap.configurations.java, {
+  type = "java",
+  request = "attach",
+  name = "Attach to Quarkus (5005)",
+  hostName = "127.0.0.1",
+  port = 5005,
+})
